46dc48d0d7ff12129b87f5d5b2a6a459e71e91da
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-08 22:09:02 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -6,5 +6,5 @@ locals {
         Terraform = "true"
     }
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-    mongodb_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
