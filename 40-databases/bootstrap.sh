efb4952edd6d76bf9f1e741d949092a5dadb9ccf
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-08 22:06:24 +0530

    terraform

---
+++ 40-databases/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+component = $1
+sudo su -
+dnf install ansible -y
+cd /home/ec2-user
+git clone https://github.com/adepusaikumar/ansible-roboshop-roles-tf.git
+
+cd ansible-roboshop-roles-tf
+ansible-playbook -e component=$component roboshop.yaml
