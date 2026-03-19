a1eda7815713a5959b6e2b8b0c46068244fbd387
adepuraju5 <rajudevops.linux@gmail.com>
2026-03-19 15:12:28 +0530

    tarraform

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
-  name    = "mongodb-${var.project}-${var.environment}"
+  name    = "mongodb-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = "1"
   records = [aws_instance.mongodb.private_ip]
