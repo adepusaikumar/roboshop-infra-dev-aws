be40478207e7b96cf902ee316dd84d218314ed23
adepuraju5 <rajudevops.linux@gmail.com>
2026-03-19 16:00:13 +0530

    tarraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -38,7 +38,6 @@ resource "terraform_data" "mongodb" {
 
 }
 
-
 resource "aws_instance" "redis" {
   ami           = local.ami_id
   instance_type = "t3.micro"
@@ -52,7 +51,6 @@ resource "aws_instance" "redis" {
     local.common_tags,
   )
 }
-
 
 resource "terraform_data" "redis" {
   triggers_replace = [
@@ -75,6 +73,45 @@ resource "terraform_data" "redis" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
       "sudo sh /tmp/bootstrap.sh redis"
+    ]
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.mysql_sg_id]
+
+  tags = merge(
+    {
+      Name = "${var.project}-${var.environment}-mysql"
+    },
+    local.common_tags,
+  )
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh mysql"
     ]
   }
 
