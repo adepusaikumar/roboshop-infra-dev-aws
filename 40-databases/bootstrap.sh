28112d4e7430d1a7aee18404d8ce311e58d5296b
adepuraju5 <rajudevops.linux@gmail.com>
2026-03-19 17:17:38 +0530

    tarraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 component=$1
+env=$2
 dnf install ansible -y
 cd /home/ec2-user
 git clone https://github.com/adepusaikumar/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component roboshop.yaml
+ansible-playbook -e component=$component -e env=$env -f roboshop.yaml
