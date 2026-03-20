c718c19f2fb8f616ff43629b89cb302a1c6e3a4a
adepuraju5 <rajudevops.linux@gmail.com>
2026-03-20 14:00:37 +0530

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -37,11 +37,12 @@ data "aws_ssm_parameter" "shipping_sg_id" {
 data "aws_ssm_parameter" "payment_sg_id" {
   name = "/${var.project}/${var.environment}/payment_sg_id"
 }
-/* 
+
 data "aws_ssm_parameter" "backend_alb_sg_id" {
   name = "/${var.project}/${var.environment}/backend_alb_sg_id"
 }
 
+/* 
 data "aws_ssm_parameter" "frontend_sg_id" {
   name = "/${var.project}/${var.environment}/frontend_sg_id"
 }
