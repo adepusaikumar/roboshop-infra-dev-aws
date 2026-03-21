73a12488c23f439d8fc3346fbec1f0bb7ec25ed0
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-21 19:03:48 +0530

    terraform

--- 50-backend-alb/parameters.tf
+++ 50-backend-alb/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "backned_alb_listener_arn" {
   name  = "/${var.project}/${var.environment}/backend_alb_listener_arn"
-  type = String
+  type = "String"
   value = aws_lb.backend_alb.arn
 }
