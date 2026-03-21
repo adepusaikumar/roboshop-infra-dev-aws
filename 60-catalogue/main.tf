9ee4bd75c38529c0d8f9b5e65974813be92b7d7c
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-21 10:01:19 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -55,9 +55,8 @@ resource "aws_ami_from_instance" "catalogue" {
   )
 }
 
-resource "aws_lb_target_group" "alb-example" {
+resource "aws_lb_target_group" "catalogue" {
   name        = "${var.project}-${var.environment}-catalogue"
-  target_type = "alb"
   port        = 8080
   protocol    = "HTTP"
   vpc_id      = local.vpc_id
