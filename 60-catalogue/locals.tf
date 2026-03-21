380100013de6a0bd5d03c8fa25716d8a1572cd6b
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-21 07:31:43 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,7 @@
 locals {
     ami_id = data.aws_ami.joindevops.id
     # backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
-    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_ids = split(", ",data.aws_ssm_parameter.private_subnet_ids.value)[0]
     
     common_tags = {
