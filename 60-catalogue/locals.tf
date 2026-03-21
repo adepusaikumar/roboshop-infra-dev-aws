25a0d9091b15d22b5346babc566229534160a3b7
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-21 07:30:53 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,6 +1,6 @@
 locals {
     ami_id = data.aws_ami.joindevops.id
-    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+    # backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
     private_subnet_ids = split(", ",data.aws_ssm_parameter.private_subnet_ids.value)[0]
     
