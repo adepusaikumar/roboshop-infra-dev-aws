10251de80462072cd2118f4280a012d807b83225
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-21 22:09:06 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -32,16 +32,18 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
   name    = "*.backend-alb-${var.environment}.${var.domain_name}" #*.backend-alb-dev.rajudevops.online
   type    = "A"
+  allow_overwrite = true
+  
 
   # load balancer details
   alias {
     name                   = aws_lb.backend_alb.dns_name
     zone_id                = aws_lb.backend_alb.zone_id
     evaluate_target_health = true
+    
   }
 }
