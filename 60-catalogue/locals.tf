1a9fb66b67ae4fb335ba7b00c73ed0a94a10a8e9
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-21 07:20:41 +0530

    terraform

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,14 @@
+locals {
+    ami_id = data.aws_ssm_parameter.ami_id.value
+    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
+    private_subnet_ids = split(", ",data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
+
+    
+}
