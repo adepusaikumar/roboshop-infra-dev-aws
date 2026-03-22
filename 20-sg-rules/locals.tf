8405c293f2aefef62b1a515113b1a98276d2e86e
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-22 14:45:18 +0530

    terraform

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -10,4 +10,6 @@ locals{
     shipping_sg_id = data.aws_ssm_parameter.shipping_sg_id.value
     payment_sg_id = data.aws_ssm_parameter.payment_sg_id.value
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+    frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+
 }
