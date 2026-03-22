829d2b9238185aedd8bf3b96eaec87308bdbba92
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-22 16:21:39 +0530

    terraform

--- 80-frontend-alb/provider.tf
+++ 80-frontend-alb/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
 backend "s3" {
     bucket = "remote-state-daws88s-roboshop-sai-dev"
-    key = "roboshop-dev-backend-frontend-alb"
+    key = "roboshop-dev-frontend-alb"
     region = "us-east-1"
     encrypt = true
     use_lockfile = true
