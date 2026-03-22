cc83b44a2b75b57fd439545991c92bd74423a635
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-22 14:11:49 +0530

    terraform

---
+++ 70-acm/varables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+  default = "rajudevops.online"
+}
+
+variable "zone_id" {
+  default = "Z101804623RU7HOQEG0G3"
+}
