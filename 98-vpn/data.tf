70a49f9758b9a28a333885409e29160e1d624663
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-29 14:22:56 +0530

    terraform

--- 98-vpn/data.tf
+++ 98-vpn/data.tf
@@ -23,4 +23,7 @@ data "aws_ssm_parameter" "openvpn_sg_id" {
     name = "/${var.project}/${var.environment}/openvpn_sg_id"
 }
 
+data "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project}/${var.environment}/public_subnet_ids"
+}
 
